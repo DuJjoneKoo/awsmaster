a8db476932ee78b67884aa679a28805e98ab1ac2
DuJjoneKoo <jmg921118@gmail.com>
2026-03-12 03:24:54 +0900

    Level 1 : 코드 작업 완료 및 주석 추가 {"status": "UP"} 확인

--- src/main/java/com/example/aws/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/aws/exception/GlobalExceptionHandler.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice // 모든 Controller의 에러를 여기서 잡음
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(Exception.class) // 모든 에러가 여기로 들어옴
     public ResponseEntity<?> handleException(Exception e) {
+
+        //에러 내용을 ERROR 레벨로 로그에 남김
         log.error("[API - ERROR] ", e);
         return ResponseEntity
                 .status(500)
