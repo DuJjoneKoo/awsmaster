46eb0e926893ec8a35da45fc8faf16267688dd1f
DuJjoneKoo <jmg921118@gmail.com>
2026-03-12 03:22:48 +0900

    Level 1 : 공통 에러 및 로그 구현완료

--- src/main/java/com/example/aws/controller/MemberController.java
+++ src/main/java/com/example/aws/controller/MemberController.java
@@ -5,9 +5,12 @@ import com.example.aws.dto.MemberResponse;
 import com.example.aws.entity.Member;
 import com.example.aws.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// log 객체를 위해 Slf4j 어노테이션 추가함
+@Slf4j
 @RequestMapping("/api/members")
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +19,12 @@ public class MemberController {
 
     @PostMapping
     public MemberResponse save(@RequestBody MemberRequest request){
+        log.info("[API - LOG] POST /api/members 요청");
         return memberService.save(request);
     }
     @GetMapping("/{memberId}")
     public MemberResponse findById(@PathVariable Long memberId) {
+        log.info("[API - LOG] GET /api/members/{} 요청", memberId );
         return memberService.findById(memberId);
     }
 }
---
+++ src/main/java/com/example/aws/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.aws.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        log.error("[API - ERROR] ", e);
+        return ResponseEntity
+                .status(500)
+               .body(e.getMessage());
+    }
+}
